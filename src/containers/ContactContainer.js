919591c0f1e1aba8a897a6c172deda589398a338
Will Bainton <baintonw@gmail.com>
2020-05-24 18:37:27 -0400

    add to clipboard in ContactCContainer

--- src/containers/ContactContainer.js
+++ src/containers/ContactContainer.js
@@ -7,18 +7,17 @@ import "../components/styles/contact.css"
 
 const ContactContainer = () => {
     const copyText = (e) => {
-
-        console.log('text copied!', 'target: ', e.target)
-        const copyText = e.target
-        // const selectedText = e.target.textArea.select()
-        // const copiedText
-        console.log('copyText: ', copyText)
+        const copiedText = e.target.innerText
+        console.log('copiedText: ', copiedText)
+        window.navigator.clipboard.writeText(copiedText)
         
     }
     return(
         <div className="contact-container">
             <div className="contact-container__email-box">
-                <h3 onClick={(e) => copyText(e)} className="contact-container__email">baintonw@gmail.com</h3>
+                <a>
+                    <h3 onClick={(e) => copyText(e)} className="contact-container__email">baintonw@gmail.com</h3>
+                </a>
             </div>
             <div className="contact-container__links">
                 
