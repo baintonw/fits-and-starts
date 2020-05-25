ac002534cfa65ef42327760f069275bc43fc7392
Will Bainton <baintonw@gmail.com>
2020-05-25 19:56:20 -0400

    contact updates with clipboard add

--- src/containers/ContactContainer.js
+++ src/containers/ContactContainer.js
@@ -5,25 +5,41 @@ import "../components/styles/contact.css"
 
 
 
-const ContactContainer = () => {
-    const copyText = (e) => {
+class ContactContainer extends React.Component {
+
+    state = {
+        emailCopied: false
+    }
+
+    copyText = (e) => {
         const copiedText = e.target.innerText
         console.log('copiedText: ', copiedText)
         window.navigator.clipboard.writeText(copiedText)
+        this.setState({
+            emailCopied: true
+        })
         
     }
-    return(
-        <div className="contact-container">
-            <div className="contact-container__email-box">
-                <a>
-                    <h3 onClick={(e) => copyText(e)} className="contact-container__email">baintonw@gmail.com</h3>
-                </a>
+
+    render() {
+        console.log('this.state: ', this.state)
+        return(
+            <div className="contact-container">
+                <div className="contact-container__prompt">{this.state.emailCopied ? 'Copied' : 'Click to copy: '}</div>
+                <div className="contact-container__email-box">
+                    
+                        <h3 onClick={(e) => this.copyText(e)} className="contact-container__email">baintonw@gmail.com</h3>
+                    
+                </div>
+                <div className="contact-container__links">
+                    
+                </div>
             </div>
-            <div className="contact-container__links">
-                
-            </div>
-        </div>
-    )
+        )
+    }
+        
+    
+    
 }
 
 export default ContactContainer
